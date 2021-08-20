fe1f4dbb22c162d7c4252f21c57a4ac1967b7d97
Yuto Hashimoto <69133317+hashimo1216@users.noreply.github.com>
2021-08-20 15:35:12 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     if Rails.env.production?
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['hashimo'] && password == ENV['1111']
-    end
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      end
     end
   end
 end
