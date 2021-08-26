b7e046ab325784f79ffcb9119b0b9cc466522219
Yuto Hashimoto <69133317+hashimo1216@users.noreply.github.com>
2021-08-26 12:45:19 +0900

    商品一覧機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_days
   belongs_to :user
-  has_one :order
+  #has_one :order
   has_one_attached :image
 
   ITEM_PRICE_REGEX = /\A[0-9]+\z/.freeze
