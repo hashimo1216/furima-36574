c00a17807c5196e0548fc919516de5923c54a8f0
Yuto Hashimoto <69133317+hashimo1216@users.noreply.github.com>
2021-09-01 17:14:53 +0900

    名前変更

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("User can't be blank")
       end
-      it 'user_idが空だと保存できないこと' do
+      it 'item_idが空だと保存できないこと' do
         @order_address.item_id = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Item can't be blank")
