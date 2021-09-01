bae9f5752d1e0616c388882ab92f673d470cdc00
Yuto Hashimoto <69133317+hashimo1216@users.noreply.github.com>
2021-09-01 13:46:20 +0900

    商品購入機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.save 
       redirect_to root_path
     else
       render :new
@@ -21,6 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+    unless user_signed_in? 
+      redirect_to root_path
+    end
   end
 
   def edit
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  def index
+    @order_address = OrderAddress.new
+    unless user_signed_in? && @item.order == nil
+      redirect_to root_path
+    end
+  end 
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token], price: @item.price)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],  
+      card: order_params[:token],   
+      currency: 'jpy'                 
+     )
+  end
+end
